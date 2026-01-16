459c03230c20566bcbde342e57957939adee1616
NirajKam <nt445281@gmail.com>
2026-01-16 09:02:40 +0545

    good

--- frontend/src/components/layout/AuthLayout.jsx
+++ frontend/src/components/layout/AuthLayout.jsx
@@ -6,11 +6,11 @@ export default function AuthLayout({ children, title, subtitle }) {
       <div className="w-full max-w-5xl bg-gray-900 rounded-2xl shadow-2xl overflow-hidden flex flex-col lg:flex-row border border-gray-800">
         
         {/* Image */}
-        <div className="hidden lg:flex w-1/2 relative flex-col items-center justify-center bg-gray-800">
-          <div className="relative w-full">
+        <div className="hidden lg:flex w-1/2 relative flex-col items-center justify-center bg-gray-800 h-full">
+          <div className="relative w-full h-full">
             <img 
-              src="https://placecats.com/300/350"
-              className="w-full h-fit object-fill drop-shadow-2xl rounded-lg transform"
+              src="/hero.jpg"
+              className="w-full h-full object-cover drop-shadow-2xl rounded-lg transform"
               alt=""
             />
           </div>
@@ -25,12 +25,10 @@ export default function AuthLayout({ children, title, subtitle }) {
               <h2 className="text-3xl font-bold text-slate-100 pb-2">Expense Tracker</h2>
               <h2 className="text-2xl font-semibold text-white">{title}</h2>
               {subtitle && <p className="text-gray-400 text-sm">{subtitle}</p>}
-              {/* The Blue Underline */}
               <div className="h-1 w-24 bg-blue-500 rounded-full mt-2"></div>
             </div>
           </div>
 
-          {/* THE FORM INPUTS GO HERE */}
           {children}
 
         </div>
